63e25cb0da4b546e164b0473cf90baef059672a5
leen-neel <and24903@gmail.com>
2022-03-29 16:22:00 +0530

    cards added

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,27 @@
 <template>
-  <q-page class="flex flex-center"> test </q-page>
+  <q-page class="flex flex-center">
+    <div class="row q-gutter-lg">
+      <q-card class="bg-primary card">
+        <q-card-section>
+          <div class="text-h2 text-center text-weight-bolder">C</div>
+        </q-card-section>
+
+        <q-card-section class="text-h4 text-weight-bold text-center">
+          15 <sup>o</sup>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="bg-primary card">
+        <q-card-section>
+          <div class="text-h2 text-center text-weight-bolder">F</div>
+        </q-card-section>
+
+        <q-card-section class="text-h4 text-weight-bold text-center">
+          15 <sup>o</sup>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
 </template>
 
 <script>
@@ -9,3 +31,10 @@ export default defineComponent({
   name: "IndexPage",
 });
 </script>
+
+<style lang="scss" scoped>
+.card {
+  width: 20vw;
+  // height: 50vh;
+}
+</style>
